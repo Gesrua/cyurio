78e4e3647ca704859c900b3d7171962ad5e65cae
Gesrua <gesrua@gmail.com>
2020-11-22 17:16:49 +0800

    RFile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CommonModule } from './common/common.module';
+import { ExtensionModule } from './extension/extension.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, ExtensionModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/common/rfile.entity.ts
@@ -0,0 +1,16 @@
+export interface RFile {
+  /** id for Nedb */
+  _id?: string;
+  /** file path */
+  path: string;
+  /** file title */
+  title: string;
+
+  /** extension type */
+  type: string;
+  /** extension type metadata */
+  metadata: any;
+
+  /** extension property */
+  prop: any;
+};
