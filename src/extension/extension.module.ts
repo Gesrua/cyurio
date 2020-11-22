580c68d5fd7208475ef885f9b3a0a6b3d4460e15
Gesrua <gesrua@gmail.com>
2020-11-22 17:27:51 +0800

    fix extension module -> service

--- src/extension/extension.module.ts
+++ src/extension/extension.module.ts
@@ -1,33 +1,9 @@
 import { Module } from '@nestjs/common';
-import { RFile } from 'src/common/rfile.entity';
 import { DatabaseService } from 'src/database/database.service';
 import { ExtensionService } from './extension.service';
-import { Text } from './type/text';
-import { Type } from './type/type';
 
 @Module({
   imports: [DatabaseService],
   providers: [ExtensionService]
 })
-export class ExtensionModule {
-  readonly types;
-  readonly fallbackType: Type;
-  readonly props;
-  readonly fallbackProps: Type;
-
-  constructor(private readonly databaseService: DatabaseService) {
-    const types = [new Text(databaseService)];
-    for(const type of types) {
-      this.types[type.type] = type;
-    }
-  }
-
-  async getType(f: RFile) {
-    for(const type of this.types) {
-      if (await type.isValid(f)) {
-        return type;
-      }
-    }
-    return this.fallbackType;
-  }
-}
+export class ExtensionModule {}
--- src/extension/extension.service.ts
+++ src/extension/extension.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { RFile } from 'src/common/rfile.entity';
+import { DatabaseService } from 'src/database/database.service';
+import { Text } from './type/text';
+import { Type } from './type/type';
 
 @Injectable()
-export class ExtensionService {}
+export class ExtensionService {
+  readonly types;
+  readonly fallbackType: Type;
+  readonly props;
+  readonly fallbackProps: Type;
+
+  constructor(private readonly databaseService: DatabaseService) {
+    const types = [new Text(databaseService)];
+    for(const type of types) {
+      this.types[type.type] = type;
+    }
+    this.fallbackType = new Type(databaseService);
+  }
+
+  async getType(f: RFile) {
+    for(const type of this.types) {
+      if (await type.isValid(f)) {
+        return type;
+      }
+    }
+    return this.fallbackType;
+  }
+}
