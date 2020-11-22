2eadc13d87da8350053a5522054b180c2967b6cb
Gesrua <gesrua@gmail.com>
2020-11-22 17:11:09 +0800

    basic database service

---
+++ src/database/database.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import Datastore = require('nedb-promises');
+
+@Injectable()
+export class DatabaseService {
+  readonly db: Datastore;
+  constructor() {
+    this.db = Datastore.create('./store.db')
+  }
+  async insert(obj) {
+    return this.db.insert(obj);
+  }
+  async find(field) {
+    return this.db.find(field);
+  }
+  async remove(obj) {
+    await this.db.remove({ _id: obj._id }, {});
+  }
+  async set(obj, prop) {
+    await this.db.update({ _id: obj._id }, { $set: prop });
+  }
+  async replace(obj) {
+    await this.db.update({ _id: obj._id }, obj);
+  }
+}
